aad06f4de57b1effa4f3666edc5b8eea78aa50ea
ayudov <yudovv@gmail.com>
2018-08-13 23:14:24 +0300

    add files

--- bot_handlers.py
+++ bot_handlers.py
@@ -31,7 +31,7 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['help'])    
 def send_help(messsage):
-    bot.send_message(message.chat.id, "Ось тобі вся основна інформація:\n", parse_mode='HTML')
+    bot.send_message(message.chat.id, "Ось тобі деяка основна інформація:\n\n", parse_mode='HTML')
     
 @bot.message_handler(content_types=["text"]) #Любой текст
 def answer_message(message):
@@ -49,31 +49,9 @@ def answer_message(message):
         for x in result:
             if message.text in x.get('All name').split(' '):
                 send = True
-                bot.send_message(message.chat.id, "Я знайшов ось кого:\n\n"+'<b>ПІБ</b>: '+'\n<b>Посилання на Телеграм: </b>'+str(x.get('TG'))+str(x.get('All name'))+'\n<b>e-mail</b>: '+str(x.get('e-mail'))+'\n<b>Номер телефону</b>: '+'0'+str(x.get('tel.'))+'\n<b>День народження</b>: '+str(x.get('Birth date'))+'\n<b>Гуртожиток</b>: '+str(x.get('info')), parse_mode='HTML')
+                bot.send_message(message.chat.id, 'Я знайшов ось кого:\n\n'+'<b>ПІБ</b>: '+str(x.get('All name'))+'\n<b>Посилання на Телеграм: </b>'+str(x.get('TG'))+'\n<b>e-mail</b>: '+str(x.get('e-mail'))+'\n<b>Номер телефону</b>: '+'0'+str(x.get('tel.'))+'\n<b>День народження</b>: '+str(x.get('Birth date'))+'\n<b>Гуртожиток</b>: '+str(x.get('info')), parse_mode='HTML')
     if send == False:
         bot.send_message(message.chat.id, "Нажаль в списку немає такої людини, ти впевнений, що все вірно ввів?")
-    #for x in result:
-    
-   # if "status - " in message.text:
-    #    #if len(input_text_array) >=4 or len(input_text_array) <3:
-     #   if len(input_text_array) != 3:
-    #        send_array.append("Пожалуйста, используйте верный формат запроса")
-    #    elif re.search('\D', input_text_array[2]):
-    #        send_array.append("Пожалуйста, номер заказа, который состоит только из цифр")
-#			#bot.send_message(message.chat.id, "Пожалуйста, номер заказа, который состоит только из цифр")
-#       else:
- #           for x  in result:
-  #              if x.get('pyxis_order_uid') == int(input_text_array[2]):
-   #                 send = True
-	#				
-     #       if send == False:
-     #           send_array.append("К сожалению, такого кода товара нет")
-
-    #else:
-    #    send_array.append("Пожалуйста, используйте верный формат запроса")
-			
-    #send_message(message.chat.id, send_text, send_array) #Отправляем сообщение
-
 
 
 if __name__ == '__main__':
