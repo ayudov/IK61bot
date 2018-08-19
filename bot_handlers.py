c4bc1b82ad2945a5718fd557096e078ed5362ae3
ayudov <yudovv@gmail.com>
2018-08-19 15:04:20 +0300

    mod bot_handlers

--- bot_handlers.py
+++ bot_handlers.py
@@ -4,9 +4,11 @@ from bot import bot
 #  Подключение Google drive
 from oauth2client.service_account import ServiceAccountCredentials
 
+
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
+client.login()
 
 sheet = client.open('IK-61 data.xlsx').sheet1
 
